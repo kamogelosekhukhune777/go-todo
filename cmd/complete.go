47a41df2873e0936f4dbc5dd3e6039a29c31234a
Kamogelo Sekhukhune <kamogelosekhukhune7@gmail.com>
2023-11-26 21:28:44 +0200

    complete??..

---
+++ cmd/complete.go
@@ -0,0 +1,48 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+// completeCmd represents the complete command
+var completeCmd = &cobra.Command{
+	Use:   "complete",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		intValue, _ := strconv.Atoi(args[0])
+		Todos.Complete(intValue)
+
+		err := Todos.Store(TodoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completeCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// completeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// completeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package main
 
-import "github.com/kamogelosekhukhune777/go-todo/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/kamogelosekhukhune777/go-todo/cmd"
+)
 
 func main() {
+	if err := cmd.Todos.Load(cmd.TodoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
 	cmd.Execute()
 }
